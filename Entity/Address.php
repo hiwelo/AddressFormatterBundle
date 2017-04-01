bdc8399c40913d95c83289df52518207c7ebd8b1
Damien Senger <damien@raccoon.studio>
2017-04-01 16:16:52 +0200

    entity: New Address entity with a link to the country entity

---
+++ Entity/Address.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CitizenKey\AddressFormatterBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="address")
+ * @ORM\Entity(repositoryClass="CitizenKey\AddressFormatterBundle\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Country
+     *
+     * @var Country
+     */
+    protected $country;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set country
+     *
+     * @param Country $country
+     *
+     * @return Address
+     */
+    public function setCountry(Country $country = null)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return Country
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
