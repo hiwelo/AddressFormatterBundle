8d1751f867f84bd59f1898fcd6264d133aaf9bbf
hiwelo <hiwelo@users.noreply.github.com>
2017-04-03 00:34:11 +0200

    configuration: AddressFormatterExtension name update

--- DependencyInjection/AddressFormattingExtension.php
+++ DependencyInjection/AddressFormattingExtension.php
@@ -12,16 +12,13 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class AddressFormattingExtension extends Extension
+class AddressFormatterExtension extends Extension
 {
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
